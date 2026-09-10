d4b3a9452df22b4f7dfee23da77311a0e64bb0df
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2026-09-10 17:00:48 +0200

    Add engines for document processing and integrate new components

--- people_gator/engines/__init__.py
+++ people_gator/engines/__init__.py
@@ -1,3 +1,10 @@
 from .base import BaseEngine, PageProcessingEngine, DocumentProcessingEngine
+from .llm import LLMBasedEngine
+
+from .face_matching import FaceMatchingEngine
 from .face_detection import FaceDetectionYoloEngine
-from .llm_prompter import LLMPrompter
+
+from .name_matching import LLMNameMatchingEngine
+from .name_detection import LLMNameDetectionEngine
+from .name_clustering import NameClusteringEngine, LLMNameClusteringCheckEngine
+from .name_description import LLMNameDescriptionEngine
